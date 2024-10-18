9d84c5bdaac9154cc36815a660eab5fc1b2c90aa
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2024-10-18 17:17:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/Hoe/69'))
 STREAM_LINK_MODE = is_enabled((environ.get('STREAM_LINK_MODE', "False")), False)
 
 #premium Users Satuts
-premium = environ.get('PREMIUM_LOGS', '-1007396439064')
+premium = environ.get('PREMIUM_LOGS', '-1001985196029')
 PREMIUM_LOGS = int(premium) if premium and id_pattern.search(premium) else None
 
 #files link shortnet
