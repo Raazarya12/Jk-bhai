071e45434c885f1d13c3968346d524b38350f316
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2025-01-27 07:18:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -65,13 +65,13 @@ premium = environ.get('PREMIUM_LOGS', '-1001985196029')
 PREMIUM_LOGS = int(premium) if premium and id_pattern.search(premium) else None
 
 #files link shortnet
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0d93e8f077f989e7c414959f4510fcbe9d772b59')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'fe42ad11c5d8487b19d045dfe7402a4db13bdd80')
 IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
 
 # verify link shortner
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/link_Streaam_Movies/30")
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/link_Streaam_Movies/28")
 VERIFY2_URL = environ.get('VERIFY2_URL', "clickspay.in")
 VERIFY2_API = environ.get('VERIFY2_API', "66b767589c06fd12b6a429e1f6a452e5577bc447")
 
