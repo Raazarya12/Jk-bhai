56732a94694141e1093d96d4a9a8b439f9b60458
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2025-03-11 15:14:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -70,7 +70,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', 'fb66a752433f2752ffdf971776859f5133
 IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
 
 # verify link shortner
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/link_Streaam_Movies/33")
 VERIFY2_URL = environ.get('VERIFY2_URL', "clickspay.in")
 VERIFY2_API = environ.get('VERIFY2_API', "66b767589c06fd12b6a429e1f6a452e5577bc447")
